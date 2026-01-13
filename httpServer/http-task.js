bf7e271c7548d629f017e19c133ec7470859afe3
arpitshukla9 <arpitshukla0901@gmail.com>
2026-01-13 21:08:06 +0530

    Add: modern version of new URL() method

--- httpServer/http-task.js
+++ httpServer/http-task.js
@@ -1,33 +1,5 @@
 const http = require("http");
 
-// const server = http.createServer((req, res) => {
-//   const parsedUrl = new URL(req.url, true);
-//   const path = parsedUrl.pathname;
-
-//   if (req.method === "GET" && path === "/") {
-//     res.writeHead(200, {
-//       "Content-Type": "text/plain",
-//     });
-//     res.end("Welcome to the Node.js HTTP Server");
-//   } else if (req.method === "GET" && path === "/about") {
-//     res.writeHead(200, {
-//       "Content-Type": "text/html",
-//     });
-//     res.end("<h1>About Page</h1><p>This is a simple Node.js HTTP server.</p>");
-//   } else if (req.method === "GET" && path === "/user") {
-//     const { name, age } = parsedUrl.query;
-//     res.writeHead(200, {
-//       "Content-Type": "application/json",
-//     });
-//     res.end(JSON.stringify({ name, age }));
-//   } else {
-//     res.writeHead(404, {
-//       "Content-Type": "text/plain",
-//     });
-//     res.end("404 Page Not Found");
-//   }
-// });
-
 
 
 const server = http.createServer((req,res) => {
